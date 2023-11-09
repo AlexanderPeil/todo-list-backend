021f6ef1731b4356a8b4e87c14d1af54c33df4ab
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-11-09 02:21:00 +0100

    added POST method to create new todos

--- todolist/serializers.py
+++ todolist/serializers.py
@@ -6,3 +6,10 @@ class TodoItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoItem
         fields = '__all__'  
+        read_only_fields = ('author',)
+
+
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return TodoItem.objects.create(**validated_data) 
--- todolist/views.py
+++ todolist/views.py
@@ -4,19 +4,27 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from todolist.models import TodoItem
-from rest_framework import serializers
-
+from rest_framework import serializers, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from todolist.serializers import TodoItemSerializer
 
 
 class TodoItemView(APIView):
-    # authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = []
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
-        todos = TodoItem.objects.all()
+        todos = TodoItem.objects.filter(author=request.user)
         serializer = TodoItemSerializer(todos, many=True)
         return Response(serializer.data)
+    
+    def post(self, request, format=None):
+         serializer = TodoItemSerializer(data=request.data, context={'request': request})
+         if serializer.is_valid():
+              serializer.save()
+              return Response(serializer.data, status=status.HTTP_201_CREATED)
+         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 class LoginView(ObtainAuthToken):
